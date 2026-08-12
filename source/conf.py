d8fcf176c38aea79c8e3a89452ccf004b07c80e0
Eric Meyer <ericallenmeyer@gmail.com>
2026-08-12 12:31:07 -0600

    Add sitemap for SEO.

--- source/conf.py
+++ source/conf.py
@@ -20,5 +20,16 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-extensions = ['sphinx_rtd_dark_mode']
+extensions = ['sphinx_rtd_dark_mode', 'sphinx_sitemap']
 default_dark_mode = True
+
+html_baseurl = 'https://docs.growbies.net/'
+html_file_suffix = ''
+html_link_suffix = ''
+
+sitemap_excludes = [
+    'search',
+    'genindex',
+    'index'
+]
+sitemap_url_scheme = '{link}'
