b8861aef4760e8f4774dafa021090572dec958af
Eric Meyer <ericallenmeyer@gmail.com>
2026-08-12 12:45:34 -0600

    Fix index.html extensionless nonsense.

--- source/conf.py
+++ source/conf.py
@@ -24,12 +24,10 @@ extensions = ['sphinx_rtd_dark_mode', 'sphinx_sitemap']
 default_dark_mode = True
 
 html_baseurl = 'https://docs.growbies.net/'
-html_file_suffix = ''
-html_link_suffix = ''
 
 sitemap_excludes = [
-    'search',
-    'genindex',
-    'index'
+    'search.html',
+    'genindex.html',
+    'index.html'
 ]
 sitemap_url_scheme = '{link}'
